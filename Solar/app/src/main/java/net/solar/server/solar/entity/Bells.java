be7639273347a2daca1b6016eac38a7e3af8bd56
zhaoyang1997 <m15226510335@163.com>
2019-05-29 18:43:28 +0800

    提交bells

---
+++ Solar/app/src/main/java/net/solar/server/solar/entity/Bells.java
@@ -0,0 +1,22 @@
+package net.solar.server.solar.entity;
+
+public class Bells {
+    private String name;
+    private String bellSrc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBellSrc() {
+        return bellSrc;
+    }
+
+    public void setBellSrc(String bellSrc) {
+        this.bellSrc = bellSrc;
+    }
+}
