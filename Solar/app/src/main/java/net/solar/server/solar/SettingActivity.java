757f9133cadfcfe301c269da1032a607c3f58738
zhaoyang1997 <m15226510335@163.com>
2019-06-03 15:17:56 +0800

    客户端

--- Solar/app/src/main/java/net/solar/server/solar/SettingActivity.java
+++ Solar/app/src/main/java/net/solar/server/solar/SettingActivity.java
@@ -1,69 +1,111 @@
 package net.solar.server.solar;
 
-import android.content.Context;
+import android.app.Service;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import net.solar.server.solar.adapter.RingAdapter;
 import net.solar.server.solar.entity.Bells;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SettingActivity extends AppCompatActivity {
-    private TextView textView;
     private ListView listView;
+    private List<Bells> bells = new ArrayList<>();
+    private RingAdapter ringAdapter;
+    private AssetFileDescriptor fileDescriptor;
+    private MediaPlayer mediaPlayer;
+    private AssetManager assetManager;
+    private int current = 1;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-
-    }
-    /**
-     * 创建铃声列表Adapter
-     */
-    private class SettingAdapter extends BaseAdapter {
-        private Context context;
-        private int itemLayout;
-        private List<Bells> bells = new ArrayList<>();
-
-        public SettingAdapter(Context context,int itemLayout,List<Bells> bells){
-            this.context = context;
-            this.itemLayout = itemLayout;
-            this.bells = bells;
-        }
-
-        @Override
-        public int getCount() {
-            return bells.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return bells.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            if(null==convertView){
-                LayoutInflater layoutInflater=LayoutInflater.from(context);
-                convertView=layoutInflater.inflate(itemLayout,null);
+        listView = findViewById(R.id.listView);
+        textView = findViewById(R.id.tv_Ring);
+        Bells bell1 = new Bells();
+        bell1.setName("铃声1");
+        Bells bell2 = new Bells();
+        bell2.setName("铃声2");
+        bells.add(bell1);
+        bells.add(bell2);
+        ringAdapter = new RingAdapter(this,R.layout.activity_setting_list,bells);
+        listView.setAdapter(ringAdapter);
+        init();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (!mediaPlayer.isPlaying()) {
+                    //启动播放
+                    mediaPlayer.start();
+                } else {
+                    mediaPlayer.pause();
+                }
+                //播放错误事件
+                mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+                    @Override
+                    public boolean onError(MediaPlayer mp, int what, int extra) {
+                        return false;
+                    }
+                });
+                //播放完成事件
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        mediaPlayer.reset();
+                        current++;
+                        if (current > 3) {
+                            current = 1;
+                        }
+                        try {
+                            fileDescriptor = assetManager.openFd("m" + current + ".mp3");
+                            mediaPlayer.setDataSource(fileDescriptor.getFileDescriptor(),
+                                    fileDescriptor.getStartOffset(),
+                                    fileDescriptor.getLength());
+                            mediaPlayer.prepare();
+                            mediaPlayer.start();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
-            textView = convertView.findViewById(R.id.setting_lv);
-//            textView.setText(bells.getName());
-            return convertView;
+        });
+    }
+
+    /**
+     * 打开震动模式
+     */
+    private void vibratorButton() {
+        Vibrator vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+        vibrator.vibrate(new long[]{0, 2000}, -1);
+    }
+
+    /**
+     * 添加铃声
+     */
+    private void init() {
+        try {
+            assetManager = getAssets();
+            fileDescriptor = assetManager.openFd("m1.mp3");
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(fileDescriptor.getFileDescriptor(),
+                    fileDescriptor.getStartOffset(),
+                    fileDescriptor.getLength());
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ Solar/app/src/main/java/net/solar/server/solar/adapter/RingAdapter.java
@@ -0,0 +1,56 @@
+package net.solar.server.solar.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import net.solar.server.solar.R;
+import net.solar.server.solar.entity.Bells;
+import net.solar.server.solar.entity.Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RingAdapter extends BaseAdapter {
+    private Activity context;
+    private int item_layout_id;
+    private List<Bells> list=new ArrayList<Bells>();
+
+    public RingAdapter(Activity context, int item_layout_id, List list) {
+        this.context = context;
+        this.item_layout_id = item_layout_id;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(null==convertView){
+            LayoutInflater layoutInflater= LayoutInflater.from(context);
+            convertView=layoutInflater.inflate(item_layout_id,null);
+        }
+        TextView textView=convertView.findViewById(R.id.tv_Ring);
+        textView.setText(list.get(position).getName());
+//        TextView daty=convertView.findViewById(R.id.daty);
+//        daty.setText(list.get(position).getDay());
+
+        return convertView;
+    }
+}
